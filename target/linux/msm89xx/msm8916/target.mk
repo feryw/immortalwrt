44df68344c2ba34336a6df1f10862df1cda9ca45
wyref <ferywu@gmail.com>
2025-08-13 15:21:01 +0700

    Update target.mk

--- target/linux/msm89xx/msm8916/target.mk
+++ target/linux/msm89xx/msm8916/target.mk
@@ -3,6 +3,7 @@ SUBTARGET:=msm8916
 BOARDNAME:=Snapdragon 410 (msm8916) based boards
 CPU_TYPE:=cortex-a7
 CPU_SUBTYPE:=neon-vfpv4
+FEATURES+=emmc
 
 define Target/Description
 	Build firmware images for msm8916 based boards.
