19c084bb7689e6ac7daa6a354a034ba0f5a5ea96
wyref <ferywu@gmail.com>
2025-08-13 13:21:18 +0700

    enable neon

--- target/linux/msm89xx/msm8916/target.mk
+++ target/linux/msm89xx/msm8916/target.mk
@@ -2,7 +2,7 @@ ARCH:=arm
 SUBTARGET:=msm8916
 BOARDNAME:=Snapdragon 410 (msm8916) based boards
 CPU_TYPE:=cortex-a7
-CPU_SUBTYPE:=vfpv4
+CPU_SUBTYPE:=neon-vfpv4
 
 define Target/Description
 	Build firmware images for msm8916 based boards.
